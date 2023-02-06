eb4137520965b40a3f9ed75b45f11e025cc565dd
Mehmet Ali AYIK <mehmetaliayikk@gmail.com>
2023-02-06 21:43:52 +0000

    GetProductWıthCategory

---
+++ Service/Services/ProductService.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Core;
+using Core.DTOs;
+using Core.Repositories;
+using Core.Services;
+using Core.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Services
+{
+    public class ProductService : Service<Product>, IProductService
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+        public ProductService(IUnitOfWork unitOfWork, IGenericRepository<Product> repository, IProductRepository productRepository, IMapper mapper) : base(unitOfWork, repository)
+        {
+            _productRepository = productRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<CustomResponseDto<List<ProductWithCategoryDto>>> GetProductsWitCategory()
+        {
+            var product = await _productRepository.GetProductsWitCategory();
+            var productsDto= _mapper.Map<List<ProductWithCategoryDto>>(product);
+            return CustomResponseDto<List<ProductWithCategoryDto>>.Success(200,productsDto);
+        }
+    }
+}
