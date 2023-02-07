171113c74129141e41f1b03acae06dd2f7feb97a
Mehmet Ali AYIK <mehmetaliayikk@gmail.com>
2023-02-07 23:17:21 +0000

    ValidateFilterAttribute

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,9 @@
+using API.Filters;
 using Core.Repositories;
 using Core.Services;
 using Core.UnitOfWorks;
 using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Repository;
 using Repository.Repositories;
@@ -15,7 +17,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers().AddFluentValidation(x => x.RegisterValidatorsFromAssemblyContaining<ProductDtoValidatior>());
+builder.Services.AddControllers(options => options.Filters.Add(new ValidateFilterAttribute())).AddFluentValidation(x => x.RegisterValidatorsFromAssemblyContaining<ProductDtoValidatior>());
+
+builder.Services.Configure<ApiBehaviorOptions>(options=>
+{
+    options.SuppressModelStateInvalidFilter= true;
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
