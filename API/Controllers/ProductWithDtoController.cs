c23137a256e7affe0202dfa98828ec45de073e9e
Mehmet Ali AYIK <mehmetaliayikk@gmail.com>
2023-02-20 23:10:08 +0000

    ProductWithDtoController

---
+++ API/Controllers/ProductWithDtoController.cs
@@ -0,0 +1,61 @@
+﻿using API.Filters;
+using Core;
+using Core.DTOs;
+using Core.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    
+    public class ProductWithDtoController : CustomBaseController
+    {
+        private readonly IProductServiceWithDto _productServiceWithDto;
+
+        public ProductWithDtoController(IProductServiceWithDto productServiceWithDto)
+        {
+            _productServiceWithDto = productServiceWithDto;
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetProductsWithCategory()
+        {
+            return CreateActionResult(await _productServiceWithDto.GetProductsWitCategory());
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {            
+            return CreateActionResult(await _productServiceWithDto.GetAllAsync());
+        }
+
+        [ServiceFilter(typeof(NotFoundFilter<Product>))]
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {            
+            return CreateActionResult(await _productServiceWithDto.GetByIdAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(ProductCreateDto productDto)
+        {            
+            return CreateActionResult(await _productServiceWithDto.AddAsync(productDto));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(ProductUpdateDto productDto)
+        {            
+            return CreateActionResult( await _productServiceWithDto.UpdateAsync(productDto));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Remove(int id)
+        {            
+            return CreateActionResult(await _productServiceWithDto.RemoveAsync(id));
+        }
+
+
+    }
+}
