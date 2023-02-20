d589c52ecf84efe46a5309f1846d179d18cd8084
Mehmet Ali AYIK <mehmetaliayikk@gmail.com>
2023-02-20 23:19:51 +0000

    SaveChange Override

--- Repository/AppDbContext.cs
+++ Repository/AppDbContext.cs
@@ -9,14 +9,14 @@ using System.Threading.Tasks;
 
 namespace Repository
 {
-    public class AppDbContext:DbContext
+    public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
 
         }
 
-        public DbSet <Category> Categories { get; set; }
+        public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductFeature> ProductFeatures { get; set; }
 
@@ -27,10 +27,10 @@ namespace Repository
             modelBuilder.Entity<ProductFeature>().HasData(new ProductFeature()
             {
                 Id = 1,
-                Color="kırmızı",
-                Height=100,
-                Width=10,
-                ProductId=1
+                Color = "kırmızı",
+                Height = 100,
+                Width = 10,
+                ProductId = 1
             },
             new ProductFeature()
             {
@@ -43,5 +43,65 @@ namespace Repository
             });
             base.OnModelCreating(modelBuilder);
         }
-    }  
-}
+
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+
+            foreach (var item in ChangeTracker.Entries())
+            {
+                if (item.Entity is BaseEntity entityReference)
+                {
+                    switch (item.State)
+                    {
+                        case EntityState.Added:
+                            {
+                                Entry(entityReference).Property(x => x.UpdatedDate).IsModified = false;
+                                entityReference.CreatedDate = DateTime.Now;
+                                break;
+                            }
+                        case EntityState.Modified:
+                            {
+                                Entry(entityReference).Property(x => x.CreatedDate).IsModified = false;
+                                entityReference.UpdatedDate = DateTime.Now;
+                                break;
+                            }
+
+                    }
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+
+        }
+
+        public override int SaveChanges()
+        {
+            foreach (var item in ChangeTracker.Entries())
+            {
+                if (item.Entity is BaseEntity entityReference)
+                {
+                    switch (item.State)
+                    {
+                        case EntityState.Added:
+                            {
+                                Entry(entityReference).Property(x => x.UpdatedDate).IsModified = false;
+                                entityReference.CreatedDate = DateTime.Now;
+                                break;
+                            }
+                        case EntityState.Modified:
+                            {
+                                Entry(entityReference).Property(x => x.CreatedDate).IsModified = false;
+                                entityReference.UpdatedDate = DateTime.Now;
+                                break;
+                            }
+
+                    }
+                }
+            }
+
+            return base.SaveChanges();
+
+        }
+    }
+}   
