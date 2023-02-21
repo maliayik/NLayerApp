03c8450d537365ed887d53380801a4066c209b40
Mehmet Ali AYIK <mehmetaliayikk@gmail.com>
2023-02-21 21:22:27 +0000

    CategoryWithDtoController

---
+++ API/Controllers/CategoryWithDtoController.cs
@@ -0,0 +1,32 @@
+﻿using Core;
+using Core.DTOs;
+using Core.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    
+    public class CategoryWithDtoController : CustomBaseController
+    {
+        private readonly IServiceWithDto<Category, CategoryDto> _categoryServiceWithDto;
+
+        public CategoryWithDtoController(IServiceWithDto<Category, CategoryDto> categoryServiceWithDto)
+        {
+            _categoryServiceWithDto = categoryServiceWithDto;
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> Get()
+        {
+            return CreateActionResult(await _categoryServiceWithDto.GetAllAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Get(CategoryDto category)
+        {
+            return CreateActionResult(await _categoryServiceWithDto.AddAsync(category));
+        }
+    }
+}
